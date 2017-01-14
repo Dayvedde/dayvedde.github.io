90841a98676d246b111267782139e8ada7254d73
David Lai <dayvedde@gmail.com>
2017-01-13 21:57:03 -0500

    Work on work section

---
+++ js/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#work-ingle').mouseover(function(){
+        $('#work-ingle-desc').animate({
+            left: '0.5em',
+            opacity: 1
+        })
+    });
+});
