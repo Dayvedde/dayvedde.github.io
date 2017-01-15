07b535299ad15948ea30030c7de8045e93cbee6d
David Lai <dayvedde@gmail.com>
2017-01-15 01:38:13 -0500

    Added text to ingle

--- js/home.js
+++ js/home.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-    $('#work-ingle').mouseover(function(){
+    $('#work-ingle').mouseenter(function(){
         $('#work-ingle-desc').animate({
-            left: '0.5em',
+            left: '1.7em',
             opacity: 1
+        })
+    });
+
+    $('#work-ingle').mouseleave(function(){
+        $('#work-ingle-desc').animate({
+            left: '-200px',
+            opacity: 0
         })
     });
 });
