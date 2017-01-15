30d54ffde2e3d429bd9946282146d1446ec35aa9
David Lai <dayvedde@gmail.com>
2017-01-15 02:05:45 -0500

    More descriptions

--- js/home.js
+++ js/home.js
@@ -4,12 +4,46 @@ $(document).ready(function() {
             left: '1.7em',
             opacity: 1
         })
-    });
-
-    $('#work-ingle').mouseleave(function(){
+    }).mouseleave(function(){
         $('#work-ingle-desc').animate({
-            left: '-200px',
+            left: '-10em',
             opacity: 0
         })
     });
+
+    $('#work-freshbooks').mouseenter(function(){
+        $('#work-freshbooks-desc').animate({
+            left: '1.7em',
+            opacity: 1
+        })
+    }).mouseleave(function(){
+        $('#work-freshbooks-desc').animate({
+            left: '-10em',
+            opacity: 0
+        })
+
+    });$('#work-metroland').mouseenter(function(){
+        $('#work-metroland-desc').animate({
+            left: '1.7em',
+            opacity: 1
+        })
+    }).mouseleave(function(){
+        $('#work-metroland-desc').animate({
+            left: '-10em',
+            opacity: 0
+        })
+    });
+
+    $('#work-ibm').mouseenter(function(){
+        $('#work-ibm-desc').animate({
+            left: '1.7em',
+            opacity: 1
+        })
+    }).mouseleave(function(){
+        $('#work-ibm-desc').animate({
+            left: '-10em',
+            opacity: 0
+        })
+    });
+
 });
